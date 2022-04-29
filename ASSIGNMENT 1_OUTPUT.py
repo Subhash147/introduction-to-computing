f3fef41187ebfd7183c309f51221593c033f1d13
Subhash147 <104625710+Subhash147@users.noreply.github.com>
2022-04-29 15:40:30 +0530

    Add files via upload

---
+++ ASSIGNMENT 1_OUTPUT.py
@@ -0,0 +1,40 @@
+enter your 1st number:
+
+50
+enter your 2nd number:
+
+40
+enter your 3rd number:
+
+60
+average of these three numbers is: 50.0
+
+enter your income=100000
+
+enter no of dependents:5
+Taxable_Income: 75000
+Tax: 15000.0
+
+enter your sid=21108029
+
+enter your name=subhash kumar
+
+enter your gender=M
+
+enter your course name=PYTHON
+
+enter your cgpa=7.4
+[21108029, 'subhash kumar', 'M', 'PYTHON', 7.4]
+
+enter_1st_student_marks:45
+
+enter 2nd student marks:86
+
+enter 3rd student marks:94
+
+enter 4th student marks:24
+
+enter 5th student marks:32
+[24.0, 32.0, 45.0, 86.0, 94.0]
+['Red', 'Green', 'White', 'Pink', 'Yellow']
+['Red', 'Green', 'White', 'Purple', 'Yellow']
